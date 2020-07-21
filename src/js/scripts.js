ced4a30da08d3b222723b061e6a6c12d37183db4
david_fernando_torres_parra <dftp93@gmail.com>
2020-07-20 20:27:09 -0500

    Add overlay menu
    update mobile menu on mobile screens

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,34 @@
-$('.cards-carousel').slick();
+
+function openMenu() {
+  
+  var btnOpen = document.getElementById("open-menu-button");
+  var btnClose = document.getElementById("close-menu-button");
+  
+  var mobileMenuOverlay = document.getElementById("menu-mobile");
+  var mobileMenu = document.getElementById("mobile-side");
+  
+  btnOpen.addEventListener("click", (e) => {
+    
+    e.preventDefault();
+    mobileMenuOverlay.classList.add("active");
+    mobileMenu.classList.add("active");
+    
+  });
+  
+  btnClose.addEventListener("click", (e) => {
+    
+    e.preventDefault();
+    mobileMenuOverlay.classList.remove("active");
+    mobileMenu.classList.remove("active");
+
+  });
+  
+};
+
+openMenu();
 
 function openQuestion(evt, questionName) {
+
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -12,4 +40,5 @@ function openQuestion(evt, questionName) {
   }
   document.getElementById(questionName).style.display = "block";
   evt.currentTarget.className += " active";
+  
 }
